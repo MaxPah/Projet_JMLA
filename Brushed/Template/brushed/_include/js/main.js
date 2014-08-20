03bca02cff4e662f52482b50587f23a60c0cec37
iTeex <pierre_lizet@hotmail.fr>
2014-08-20 11:05:06 +0200

    Plein de modifications
    Tout ça est sur le serveur (sauf brouillon).

--- Brushed/Template/brushed/_include/js/main.js
+++ Brushed/Template/brushed/_include/js/main.js
@@ -77,10 +77,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''}  
+											{image : 'fileadmin/Templates/_include/img/slider-images/image01.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''},
+											{image : 'fileadmin/Templates/_include/img/slider-images/image02.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''},
+											{image : 'fileadmin/Templates/_include/img/slider-images/image03.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''},
+											{image : 'fileadmin/Templates/_include/img/slider-images/image04.jpg', title : '<div class="slide-content">Architecte Jean-Marc Lefebvre</div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
@@ -212,38 +212,6 @@ BRUSHED.contactForm = function(){
 	});
 }
 
-
-/* ==================================================
-   Twitter Feed
-================================================== */
-
-BRUSHED.tweetFeed = function(){
-	
-	var valueTop = -64; // Margin Top Value
-	
-    $("#ticker").tweet({
-          modpath: '_include/js/twitter/',
-          username: "Bluxart", // Change this with YOUR ID
-          page: 1,
-          avatar_size: 0,
-          count: 10,
-		  template: "{text}{time}",
-		  filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
-          loading_text: "loading ..."
-	}).bind("loaded", function() {
-	  var ul = $(this).find(".tweet_list");
-	  var ticker = function() {
-		setTimeout(function() {
-			ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
-				$(this).detach().appendTo(ul).removeAttr('style');
-			});	
-		  ticker();
-		}, 5000);
-	  };
-	  ticker();
-	});
-	
-}
 
 
 /* ==================================================
@@ -405,7 +373,7 @@ $(document).ready(function(){
 	Modernizr.load([
 	{
 		test: Modernizr.placeholder,
-		nope: '_include/js/placeholder.js', 
+		nope: 'fileadmin/Templates/_include/js/placeholder.js', 
 		complete : function() {
 				if (!Modernizr.placeholder) {
 						Placeholders.init({
@@ -439,7 +407,6 @@ $(document).ready(function(){
 	BRUSHED.filter();
 	BRUSHED.fancyBox();
 	BRUSHED.contactForm();
-	BRUSHED.tweetFeed();
 	BRUSHED.scrollToTop();
 	BRUSHED.utils();
 	BRUSHED.accordion();
